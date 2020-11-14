12a020732e4ef2f8c2027776ba1c9d72a60c0c5b
Melesio Albavera <melesio.albavera@acecharterhigh.org>
2020-11-13 17:52:53 -0800

    Example using the Lanterna Library. New Maven Dependency.

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Main
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
-		System.out.println("Greetings from Melesio.");
+		 Terminal terminal = new DefaultTerminalFactory().createTerminal();
+		 Screen screen = new TerminalScreen(terminal);
+
+		 TextGraphics textGraphics = screen.newTextGraphics();
+
+		 screen.startScreen();
+
+		 textGraphics.putString(0,0, "Greetings from Melesio.");
+		 screen.refresh();
+		 screen.readInput();
 	}
 }
