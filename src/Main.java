037eec14b4f2efb7e46d01afe09dc962a6d46481
Adam <adam.azcorra@acecharterhigh.org>
2020-12-04 17:57:09 -0800

    Began DoesPieceFit Method.

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,11 @@ import java.io.IOException;
 
 public class Main
 {
+	int nFieldWidth = 12;
+
 	public static void main(String[] args) throws IOException
 	{
+		/*
 		 Terminal terminal = new DefaultTerminalFactory().createTerminal();
 		 Screen screen = new TerminalScreen(terminal);
 
@@ -20,5 +23,46 @@ public class Main
 		 textGraphics.putString(0,0, "Greetings. This a test.");
 		 screen.refresh();
 		 screen.readInput();
+		 */
 	}
+
+	int nFieldHeight = 18;
+	char pField;
+	String tetromino[7];
+	int Rotate(int px, int py, int r)
+	{
+		int pi = 0;
+
+
+		return pi;
+	}
+
+	public boolean DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+	{
+		for(int px = 0; px < 4; px++)
+		{
+			for(int py = 0; py < 4; py++)
+			{
+				int pi = Rotate(px, py, nRotation);
+				int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+
+				if(nPosX + px >= 0 && nPosX + px < nFieldWidth)
+				{
+					if(nPosY + py >= 0 && nPosY + py < nFieldHeight)
+					{
+						if(nPosY + py >= 0 && nPosY + py < nFieldHeight)
+						{
+							if(tetromino[nTetromino][pi] != '.' && pField[fi] !=0 )
+							{
+								return false;
+							}
+
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
+
 }
